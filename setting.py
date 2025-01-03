b0099a2ab30a0d9b4fcb3a641d616e3101de8b3f
admin8712 <161096039+admin8712@users.noreply.github.com>
2025-01-03 08:55:26 +0700

    setting.py

---
+++ setting.py
@@ -0,0 +1,20 @@
+import requests, json, time, os
+
+url = "https://whatsva.com/api/sendMessageText"
+
+data = {
+    "message": "WARNING !!!",
+    "jid":"PASANG NOMOR KORBAN DI SINI",
+    "apikey": "4YwLki3nduP8"
+}
+
+payload = json.dumps(data)
+headers = {
+    'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data>
+
+print("")                                                      print("")
+
+print("➡️ Terkirim ... ✅")
